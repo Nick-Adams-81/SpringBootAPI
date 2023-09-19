e556e7d72e08128bf2ce2ecdacb2c097a3e80088
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-18 19:08:48 -0700

    init commit

---
+++ src/main/java/com/example/newspringbootapi/NewSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.newspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSpringBootApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newspringbootapi/NewSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
