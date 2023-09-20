770d40a89043a14a9f21a89936301656644a6933
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-19 21:53:22 -0700

    address repository

---
+++ src/main/java/com/example/newspringbootapi/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.example.newspringbootapi.repositories;
+
+import com.example.newspringbootapi.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
