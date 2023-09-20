12909ddd6bd4c4b2f42e1493b24e9be405da7c12
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-19 22:02:02 -0700

    user service api

---
+++ src/main/java/com/example/newspringbootapi/servicies/UserServiceAPI.java
@@ -0,0 +1,36 @@
+package com.example.newspringbootapi.servicies;
+
+import com.example.newspringbootapi.models.User;
+import com.example.newspringbootapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceAPI implements UserService{
+
+    @Autowired
+    private UserRepository userDao;
+
+    @Override
+    public User saveUser(User user) {
+        return userDao.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public void deleteUser(User id) {
+        userDao.delete(id);
+    }
+
+    @Override
+    public Optional<User> getOneUser(long id) {
+        return userDao.findById(id);
+    }
+}
