3cf7585877ad5a0bbb3cffec5d155b1be66229c8
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-19 19:00:28 -0700

    address table

---
+++ src/main/java/com/example/newspringbootapi/models/Address.java
@@ -0,0 +1,78 @@
+package com.example.newspringbootapi.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String streetName;
+
+    @Column
+    private String streetNumber;
+
+    @Column
+    private String cityName;
+
+    @Column
+    private String stateName;
+
+    @Column
+    private int zipCode;
+
+    @OneToOne
+    private User user;
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
