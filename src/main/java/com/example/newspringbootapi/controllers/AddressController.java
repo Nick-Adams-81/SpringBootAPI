07c19e590ad2eca442fad472452aba534e6dd6d6
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-21 13:46:51 -0700

    added get one and delete address routes

--- src/main/java/com/example/newspringbootapi/controllers/AddressController.java
+++ src/main/java/com/example/newspringbootapi/controllers/AddressController.java
@@ -5,12 +5,11 @@ import com.example.newspringbootapi.models.Address;
 import com.example.newspringbootapi.servicies.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AddressController {
@@ -24,11 +23,30 @@ public class AddressController {
         return addressService.getAllAddresses();
     }
 
+    @GetMapping("/one-address/{id}")
+    @ResponseBody
+    public Optional<Address> getOnePost(@PathVariable long id) {
+        return addressService.getOneAddress(id);
+    }
+
     @PostMapping("/save-address")
     @ResponseBody
     public String saveAddress(@RequestBody Address address) {
         addressService.saveAddress(address);
         return "Address saved";
     }
+
+    @DeleteMapping("/delete-address/{id}")
+    @ResponseBody
+    public String deleteAddress(@PathVariable Address id) {
+        try{
+            addressService.deleteAddress(id);
+        } catch(ErrorResponseException e) {
+            e.printStackTrace();
+        }
+        return "Address deleted";
+    }
+
+
 
 }
--- src/main/java/com/example/newspringbootapi/servicies/PosstsServiceAPI.java
+++ src/main/java/com/example/newspringbootapi/servicies/PosstsServiceAPI.java
@@ -1,28 +1,34 @@
 package com.example.newspringbootapi.servicies;
 
 import com.example.newspringbootapi.models.Posts;
+import com.example.newspringbootapi.repositories.PostsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
 public class PosstsServiceAPI implements PostsService{
+
+    @Autowired
+    private PostsRepository postDao;
+
     @Override
     public void savePost(Posts post) {
-
+        postDao.save(post);
     }
 
     @Override
     public List<Posts> getAllPosts() {
-        return null;
+        return postDao.findAll();
     }
 
     @Override
     public void deletePost(Posts id) {
-
+        postDao.delete(id);
     }
 
     @Override
     public Optional<Posts> getOnePost(long id) {
-        return Optional.empty();
+        return postDao.findById(id);
     }
 }
