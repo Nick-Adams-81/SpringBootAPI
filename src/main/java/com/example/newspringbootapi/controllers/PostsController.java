f89c59f92cf0656ad5a1f96e4f024996d0d12f98
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-21 18:23:20 -0700

    post service added

--- src/main/java/com/example/newspringbootapi/controllers/PostsController.java
+++ src/main/java/com/example/newspringbootapi/controllers/PostsController.java
@@ -1,4 +1,12 @@
 package com.example.newspringbootapi.controllers;
 
+import com.example.newspringbootapi.servicies.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PostsController {
+
+    @Autowired
+    private PostsService postService;
 }
