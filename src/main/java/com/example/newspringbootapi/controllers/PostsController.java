e54de43b858f2177dd46338a2ff9bcb8cbb74014
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-23 18:03:20 -0700

    added delete route for posts controller

--- src/main/java/com/example/newspringbootapi/controllers/PostsController.java
+++ src/main/java/com/example/newspringbootapi/controllers/PostsController.java
@@ -4,12 +4,10 @@ import com.example.newspringbootapi.models.Posts;
 import com.example.newspringbootapi.servicies.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostsController {
@@ -23,11 +21,23 @@ public class PostsController {
         return postService.getAllPosts();
     }
 
+    @GetMapping("/get-posts/{id}")
+    @ResponseBody
+    public Optional<Posts> getOnePost(@PathVariable long id) {
+        return postService.getOnePost(id);
+    }
 
     @PostMapping("/save-post")
     @ResponseBody
     public String savePost(@RequestBody Posts post) {
         postService.savePost(post);
         return "New Post added";
+    }
+
+    @DeleteMapping("/delete-post/{id}")
+    @ResponseBody
+    public String deletePost(@PathVariable Posts id) {
+        postService.deletePost(id);
+        return "Post with id: " + id.getId() + " has been deleted";
     }
 }
