9353033482ef2fa78640b4c29043dd13947e6f88
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-28 11:35:41 -0700

    removed unused import from address controller

--- src/main/java/com/example/newspringbootapi/controllers/AddressController.java
+++ src/main/java/com/example/newspringbootapi/controllers/AddressController.java
@@ -5,7 +5,6 @@ import com.example.newspringbootapi.models.Address;
 import com.example.newspringbootapi.servicies.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
